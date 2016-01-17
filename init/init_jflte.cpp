435606e0da085b2d51f9736e19a2c2b0d4d21fb4
Mattia D'Alleva <antaresone@antaresone.eu>
2016-01-17 19:10:57 +0100

    Init: RAF: better property nomenclature (3/3)
    https://github.com/AOSP-JF-MM/platform_frameworks_opt_telephony/commit/69537b22e80f1b7afce39b788e49260b2037adf5
    https://github.com/AOSP-JF-MM/platform_packages_apps_CarrierConfig/commit/f8748ef09410a4918cd540ed226349304cb1781c

--- init/init_jflte.cpp
+++ init/init_jflte.cpp
@@ -177,7 +177,7 @@ void gsm_properties()
 {
     property_set("telephony.lteOnGsmDevice", "1");
     property_set("ro.telephony.default_network", "9");
-    property_set("ro.ril.device_family", "gsm");
+    property_set("telephony.radioAccessFamily", "gsm");
     property_set("rild.libpath", "/system/lib/libril-qc-qmi-1.so");
 }
 
@@ -189,7 +189,7 @@ void cdma_properties(char cdma_sub[])
     property_set("persist.radio.snapshot_enabled", "1");
     property_set("persist.radio.snapshot_timer", "22");
     property_set("telephony.lteOnCdmaDevice", "1");
-    property_set("ro.ril.device_family", "cdma");
+    property_set("telephony.radioAccessFamily", "cdma");
     property_set("rild.libpath", "/system/lib/libsec-ril.so");
 }
 
